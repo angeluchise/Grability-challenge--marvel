93b701e20b3547bcef82698439a887bc2e7a6455
angelperez <angel.per.cho@gmail.com>
2016-12-31 19:03:17 -0400

    feature/metodolyBEM

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,7 +7,7 @@
  * # AboutCtrl
  * Controller of the pruebaApp
  */
-angular.module('pruebaApp')
+angular.module('changelle-marvelApp')
   .controller('AboutCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
