17d58942f57c139abbea8e6ba6b52abd10371802
angelperez <angel.per.cho@gmail.com>
2017-01-03 16:12:47 -0400

    fix/apimarvel

--- app/scripts/config/config.js
+++ app/scripts/config/config.js
@@ -3,7 +3,6 @@ var key = '8c098e6996b2d32729ad977e5aad530b';
 var hash = '6040c305bcd4d0d24d1cced0103262be';
 var td = new Date();
 var timestap = '1483397108829000';
-console.log(timestap);
 var url = baseUrl + 'characters?ts='+ timestap +'&apikey='+ key +'&hash='+hash;
 angular.module('changelle-marvelApp')
 
