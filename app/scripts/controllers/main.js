795b0c797a08ec16ff6d56484f4352df844ab154
angelperez <angel.per.cho@gmail.com>
2017-01-04 17:38:51 -0400

    feature/pagination x

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,13 +7,52 @@
  * # MainCtrl
  * Controller of the pruebaApp
  */
-angular.module('changelle-marvelApp')
-  .controller('MainCtrl', function ($scope,api_marvel) {
-    $(document).ready(function() {
-      $('select').material_select();
-    });
+
+  var app = angular.module('changelle-marvelApp');
+
+  app.controller('MainCtrl',  function($scope,api_marvel) {
+    $scope.currentPage = 0;
+
+    $scope.pageSize = 10; // Esta la cantidad de registros que deseamos mostrar por página
+    $scope.pages = [];
     api_marvel.get(function (data){
       $scope.characters = data.data.results;
-      console.log($scope.characters);
     });
+    $scope.configPages = function() {
+      api_marvel.get(function (data){
+        $scope.characters = data.data.results;
+        $scope.pages.length = 0;
+        var ini = $scope.currentPage - 4;
+        var fin = $scope.currentPage + 5;
+        if (ini < 1) {
+          ini = 1;
+          if (Math.ceil($scope.characters.length / $scope.pageSize) > 10) fin = 10;
+          else fin = Math.ceil($scope.characters.length / $scope.pageSize);
+        } else {
+          if (ini >= Math.ceil($scope.characters.length / $scope.pageSize) - 10) {
+            ini = Math.ceil($scope.characters.length / $scope.pageSize) - 10;
+            fin = Math.ceil($scope.characters.length / $scope.pageSize);
+          }
+        }
+        if (ini < 1) ini = 1;
+        for (var i = ini; i <= fin; i++) {
+          $scope.pages.push({ no: i });
+        }
+        if ($scope.currentPage >= $scope.pages.length)
+          $scope.currentPage = $scope.pages.length - 1;
+      });
+    };
+    
+
+
+    $scope.setPage = function(index) {
+      $scope.currentPage = index - 1;
+    };
+
+    $scope.configPages();
+  }).filter('startFromGrid', function() {
+     return function(input, start) {
+        start = +start;
+        return input.slice(start);
+     };
   });
